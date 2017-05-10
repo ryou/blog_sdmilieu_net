085635f16dab195515798027710d090767120dc9
Your Name <you@example.com>
2017-05-10 22:32:58 +0900

    first commit

---
+++ html/public/_scripts/generateArticleList.php
@@ -0,0 +1,126 @@
+<?php
+
+require_once "./libs/Parsedown.php";
+
+mb_internal_encoding("UTF-8");
+
+{
+  $currentDir = dirname(__FILE__);
+  $postsDir = $currentDir . "/../posts";
+
+  /***********************************************************************
+  対象記事ディレクトリのメタ情報を返却
+  メタファイルが存在しないまたは不正（created_atが存在しない）など、
+  取得に失敗した場合falseを返却
+  **********************************************************************/
+  function getArticleMetaFile($articleDir)
+  {
+    $filePath = $articleDir . "/meta.json";
+
+    if (is_file($filePath))
+    {
+      $metaJSON = file_get_contents($filePath);
+      $meta = json_decode($metaJSON);
+
+      if (isset($meta->created_at))
+      {
+        return $meta;
+      }
+    }
+
+    return false;
+  }
+
+  /***********************************************************************
+  対象記事ディレクトリの記事のタイトルと要約を取得
+  情報が存在しない、又は不正な場合はfalseを返却
+  **********************************************************************/
+  function getArticleInfo($articleDir)
+  {
+    $info = [
+      "title" => "",
+      "excerpt" => ""
+    ];
+
+    $filePath = $articleDir . "/README.md";
+
+    if (is_file($filePath))
+    {
+      $mdDataArray = file($filePath);
+
+      // 記事タイトルの取得
+      {
+        // 改行コードを削除
+        $tmpArray = array_map(function($item) {
+          return str_replace(["\r\n", "\r", "\n"], "", $item);
+        }, $mdDataArray);
+        $line01 = $tmpArray[0];
+
+        // TODO:substrで「２文字目から取得」って感じで、柔軟性がない。
+        // （#の後に空白２個とか入れてしまうと変になる）のでもうちょっとなんとか
+        $info["title"] = mb_substr($line01, 2);
+      }
+
+      // 要約の取得
+      {
+        unset($mdDataArray[0]);
+        $mdData = implode($mdDataArray);
+
+        $pd = new Parsedown();
+        $html = $pd->text($mdData);
+        $tagRemovedStr = strip_tags($html);
+        $tagRemovedStr = str_replace(["\r\n", "\r", "\n"], "", $tagRemovedStr);
+
+        $info["excerpt"] = mb_substr($tagRemovedStr, 0, 100);
+      }
+
+      return $info;
+    }
+
+    return false;
+  }
+
+  /***********************************************************************
+  postsディレクトリ直下にある記事のディレクトリ一覧を取得
+  **********************************************************************/
+  function getArticleDatas($postsDir)
+  {
+    $articleDatas = [];
+
+    $items = scandir($postsDir);
+    $items = array_filter($items, function($item) {
+      return ($item !== "." && $item !== "..");
+    });
+
+    foreach ($items as $item) {
+      $fullPath = $postsDir . "/" . $item;
+
+      if (is_dir($fullPath)) {
+        $meta = getArticleMetaFile($fullPath);
+        if ($meta !== false) {
+          $info = getArticleInfo($fullPath);
+          if ($info !== false) {
+            $articleDatas[] = [
+              "perma_link" => $item,
+              "created_at" => $meta->created_at,
+              "title" => $info["title"],
+              "excerpt" => $info["excerpt"]
+            ];
+          }
+        }
+      }
+    }
+
+    return $articleDatas;
+  }
+
+  $jsonObj = [
+    "meta" => [
+      "status" => 200
+    ],
+    "data" => getArticleDatas($postsDir)
+  ];
+
+  $json = json_encode($jsonObj);
+  file_put_contents($postsDir . '/articles.json', $json);
+}
---
+++ html/public/assets/js/main.js
@@ -0,0 +1,182 @@
+;(function() {
+  var PagerComponent = {
+    props: ['current', 'total'],
+    template: '#pager-component-template',
+    computed: {
+      prevIsExist: function() {
+        return (this.current > 0);
+      },
+      nextIsExist: function() {
+        return (this.current+2 < this.total);
+      },
+      prevUrl: function() {
+        return '/page/' + (this.current - 1);
+      },
+      nextUrl: function() {
+        return '/page/' + (this.current + 1);
+      }
+    },
+    methods: {
+      showPrev: function() {
+        this.$emit('prev');
+      },
+      showNext: function() {
+        this.$emit('next');
+      }
+    }
+  };
+  var IndexComponent = {
+    data: function() {
+      return {
+        currentPage: 0,
+        count: 10
+      };
+    },
+    props: ['articles'],
+    template: '#index-component-template',
+    computed: {
+      dispArticles: function() {
+        return this.articles.slice(this.count*this.currentPage, this.count*(this.currentPage+1));
+      },
+      totalPage: function() {
+        return (this.articles.length / this.count) + 1;
+      }
+    },
+    methods: {
+      prevPage: function() {
+        this.currentPage--;
+      },
+      nextPage: function() {
+        this.currentPage++;
+      }
+    },
+    watch: {
+      '$route': function(to, from) {
+        if (typeof to.params.index === 'undefined') {
+          this.currentPage = 0;
+        } else {
+          this.currentPage = parseInt(to.params.index);
+        }
+      }
+    },
+    components: {
+      'link-component': {
+        props: ['article'],
+        template: '#link-component-template',
+        computed: {
+          url: function() {
+            return '/posts/';
+          },
+          date: function() {
+            var date = new Date(
+              this.article.date.slice(0, 4),
+              this.article.date.slice(4, 6)-1,
+              this.article.date.slice(6, 8)
+            );
+            return date;
+          }
+        }
+      },
+      'pager-component': PagerComponent
+    },
+    mounted: function() {
+      document.title = 'index';
+      if (typeof this.$route.params.index === 'undefined') {
+        this.currentPage = 0;
+      } else {
+        this.currentPage = parseInt(this.$route.params.index);
+      }
+    }
+  };
+  var PostsComponent = {
+    data: function() {
+      return {
+        content: '',
+        date: ''
+      };
+    },
+    template: '#posts-component-template',
+    computed: {
+      date: function() {
+        return (this.$route.params.perma_link.split('__'))[0];
+      },
+      title: function() {
+        return (this.$route.params.perma_link.split('__'))[1];
+      },
+      dateObj: function() {
+        var date = new Date(
+          this.date.slice(0, 4),
+          this.date.slice(4, 6)-1,
+          this.date.slice(6, 8)
+        );
+        return date;
+      }
+    },
+    mounted: function() {
+      document.title = this.title;
+
+      var self = this;
+      $.ajax({
+        type: 'GET',
+        url: './README.md',
+      })
+      .then(
+        function(res) {
+          self.content = marked(res);
+        },
+        function() {
+          console.log('error');
+        }
+      );
+      $.getJSON('./meta.json')
+        .done(function(json) {
+          self.date = json.created_at;
+        });
+    }
+  };
+  var NotFoundComponent = {
+    template: '<div>not found.</div>',
+    mounted: function() {
+      document.title = 'not found';
+    }
+  };
+
+  var app = new Vue({
+    data: {
+      articles: []
+    },
+    el: '#app',
+    router: new VueRouter({
+      mode: 'history',
+      routes: [
+        { path: '/', component: IndexComponent },
+        { path: '/page/:index/', component: IndexComponent },
+        { path: '/posts/:perma_link/', component: PostsComponent },
+        { path: '*', component: NotFoundComponent }
+      ]
+    }),
+    created: function() {
+      var self = this;
+      $.getJSON('/posts/articles.json')
+        .done(function(json) {
+          if (json.meta.status !== 200) {
+            alert('記事リストの取得に失敗しました。');
+            return;
+          }
+          json.data.forEach(function(e, i, a) {
+            var obj = {
+              url: '/posts/' + e.perma_link + '/',
+              date: e.created_at,
+              title: e.title,
+              excerpt: e.excerpt
+            };
+            self.articles.push(obj);
+          });
+        })
+        .fail(function() {
+          alert('記事リストの取得に失敗しました。');
+          return;
+        });
+    }
+  });
+})();
