e07dd7fc156cd71e56df015fa5077ed7fba9f92a
admin <admin@admin-no-iMac.local>
2017-08-15 14:57:45 +0900

    記事編集

---
+++ html/public/posts/js_audio_analyzer/demo/AudioController.js
@@ -0,0 +1,100 @@
+/*
+ * AudioController
+ *
+----------------------------------------------------------*/
+var AudioController = (function() {
+  // constructor
+  var AudioController = function() {
+    this._context = new AudioContext();
+    this._analyzer = this._context.createAnalyser();
+    this._gainNode = this._context.createGain();
+    this._startTime = null;
+    this._source = null;
+
+    this._analyzer.connect(this._gainNode);
+    this._gainNode.connect(this._context.destination);
+  };
+
+  // methods
+  var proto = AudioController.prototype;
+
+  proto.load = function(buffer, callback) {
+    var self = this;
+    self._context.decodeAudioData(buffer, function(decodedBuffer) {
+      if(self._source) {
+        self._source.stop();
+      }
+　
+      self._source = self._context.createBufferSource();
+
+      self._source.buffer = decodedBuffer;
+      self._source.connect(self._analyzer);
+
+      callback();
+    });
+  };
+
+  proto.play = function(time) {
+    if (this._source) {
+      if (typeof time === 'undefined') {
+        time = 0;
+      }
+
+      this._startTime = Date.now()/1000 - time;
+      this._source.start(time);
+    }
+  };
+
+  proto.pause = function() {
+
+  };
+
+  proto.stop = function() {
+    if (this.source) {
+      this.source.stop();
+    }
+  };
+
+  proto.seek = function() {
+
+  };
+
+  proto.volume = function(volume) {
+    if (typeof volume === 'undefined') {
+      return this._gainNode.gain.value;
+    }
+
+    this._gainNode.gain.value = volume;
+  };
+
+  proto.audio = function() {
+    var self = this;
+
+    if (self._source === null) {
+      return null;
+    }
+
+    var audio = {
+      currentTime: (function() {
+        if (self._startTime === null) {
+          return null;
+        }
+
+        return Date.now()/1000 - self._startTime;
+      })(),
+      totalTime: self._source.buffer.duration,
+      status: null
+    };
+
+    return audio;
+  };
+
+  proto.getSpectrums = function() {
+    var spectrums = new Uint8Array(this._analyzer.frequencyBinCount);
+    this._analyzer.getByteFrequencyData(spectrums);
+
+    return spectrums;
+  };
+
+  return AudioController;
+})();
--- html/public/posts/js_audio_analyzer/demo/main.js
+++ html/public/posts/js_audio_analyzer/demo/main.js
@@ -1,83 +1,57 @@
 $(function() {
-  var source, animationId;
-  var audioContext = new AudioContext;
-  var fileReader   = new FileReader;
-
+  var animationId;
   var $svg = $('.m-svg01');
   var $line = $svg.find('.m-svg01__line');
 
-  var gainNode = audioContext.createGain();
-  var analyser = audioContext.createAnalyser();
-  analyser.fftSize = 512;
-  gainNode.gain.value = 0.5;
-  // analyser => gainNode => destinationとつなぐ
-  gainNode.connect(audioContext.destination)
-  analyser.connect(gainNode);
+  var audioController = new AudioController();
 
-  // ファイル読み込み後処理
-  fileReader.addEventListener('load', function(e) {
-    audioContext.decodeAudioData(fileReader.result, function(buffer) {
-      if(source) {
-        source.stop();
-        cancelAnimationFrame(animationId);
-      }
-　
-      source = audioContext.createBufferSource();
-
-      source.buffer = buffer;
-      source.connect(analyser);
-      source.start(0);
-　
-      animationId = requestAnimationFrame(render);
-    });
-  });
-　
-  // 描画更新処理
   var render = function() {
-    var spectrums = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(spectrums);
+    var spectrums = audioController.getSpectrums();
+    var waveWidth = 640 / spectrums.length;
 
-    var points = [];
     var pointStr = '0,0 ';
-    var tmp;
-    for(var i=0, len=(spectrums.length*5)/6; i<len; i++){
-      tmp = (spectrums[i]);
-      tmp = (tmp < 0) ? 0 : tmp;
-      pointStr += i*3 + ',' + tmp + ' ';
+    for(var i=0, len=spectrums.length; i<len; i++){
+      pointStr += i*waveWidth + ',' + spectrums[i] + ' ';
     }
     pointStr += '640,0';
     $line.attr('points', pointStr);
-　
+
     animationId = requestAnimationFrame(render);
   };
 
   // ファイル選択処理
   (function() {
-    var $components = $('.m-file-upload01');
+    var fileReader = new FileReader();
+    var $modUpload = $('.m-file-upload01');
+    var $main = $modUpload.find('.m-file-upload01__main');
+    var $btn = $modUpload.find('.m-file-upload01__btn');
+    var $fileName = $modUpload.find('.m-file-upload01__file-name');
+    var $input = $modUpload.find('.m-file-upload01__input');
 
-    $components.each(function() {
-      var $component = $(this);
-      var $main = $component.find('.m-file-upload01__main');
-      var $btn = $component.find('.m-file-upload01__btn');
-      var $fileName = $component.find('.m-file-upload01__file-name');
-      var $input = $component.find('.m-file-upload01__input');
 
-      $main.add('.m-overlay01').on('click', function() {
-        $input.trigger('click');
+    fileReader.addEventListener('load', function(e) {
+      audioController.load(fileReader.result, function() {
+        audioController.play();
 
-        return false;
+        animationId = requestAnimationFrame(render);
       });
+    });
 
-      $input.on('change', function(e) {
-        var files = e.target.files;
-        if (files.length > 0) {
-          $('.m-overlay01').addClass('is-hidden');
-          var fileName = files[0].name;
-          $fileName.text(fileName);
+    $main.add('.m-overlay01').on('click', function() {
+      $input.trigger('click');
 
-          fileReader.readAsArrayBuffer(e.target.files[0]);
-        }
-      });
+      return false;
+    });
+
+    $input.on('change', function(e) {
+      var files = e.target.files;
+      if (files.length > 0) {
+        $('.m-overlay01').addClass('is-hidden');
+        var fileName = files[0].name;
+        $fileName.text(fileName);
+
+        fileReader.readAsArrayBuffer(e.target.files[0]);
+      }
     });
   })();
 });
