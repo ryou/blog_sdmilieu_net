e2052a4fbe0630569cbe296d238f12bb16b99dda
admin <admin@admin-no-iMac.local>
2017-08-16 11:42:37 +0900

    ソース編集

--- html/public/posts/js_audio_analyzer/demo/AudioController.js
+++ html/public/posts/js_audio_analyzer/demo/AudioController.js
@@ -3,37 +3,79 @@
  *
 ----------------------------------------------------------*/
 var AudioController = (function() {
-  // constructor
+
+  /***********************************************************************
+  コンストラクタ
+  **********************************************************************/
   var AudioController = function() {
     this._context = new AudioContext();
     this._analyzer = this._context.createAnalyser();
     this._gainNode = this._context.createGain();
-    this._startTime = null;
+    this._musicBuffer = null;
     this._source = null;
 
+    /* ステータス
+     * blank: 音声読み込み前
+     * pause: 停止中
+     * play : 再生中
+    ----------------------------------------------------------*/
+    this._status = 'blank';
+
+    /* _statusが'play'の時のみ使用
+     * 音声の開始UnixTimeを秒で保存
+     * ex: 音声がx秒目から開始された場合、「音声開始UnixTime - x」が保存される。
+    ----------------------------------------------------------*/
+    this._startTime = null;
+
+    /* _statusが'pause'の時のみ使用
+     * 音声の現在再生時間を保存
+     * ex: 音声のx秒目で停止したなら、xが保存される。
+    ----------------------------------------------------------*/
+    this._currentTime = null;
+
     this._gainNode.connect(this._context.destination);
+    this._gainNode.connect(this._analyzer);
   };
 
-  // methods
+
+  /***********************************************************************
+  メソッド
+  **********************************************************************/
   var proto = AudioController.prototype;
 
+  /* 音声データの再生準備
+   * buffer: デコード済音声データ
+  ----------------------------------------------------------*/
+  proto._setup = function(buffer) {
+    if(this._source) {
+      this._source.stop();
+    }
+　
+    this._source = this._context.createBufferSource();
+
+    this._source.buffer = buffer;
+    this._source.connect(this._gainNode);
+  };
+
+  /* デコード前音声データの読み込み
+   * buffer: デコード前音声データ
+   * callback: 読み込み完了時に実行されるコールバック
+  ----------------------------------------------------------*/
   proto.load = function(buffer, callback) {
     var self = this;
     self._context.decodeAudioData(buffer, function(decodedBuffer) {
-      if(self._source) {
-        self._source.stop();
-      }
-　
-      self._source = self._context.createBufferSource();
-
-      self._source.buffer = decodedBuffer;
-      self._source.connect(self._analyzer);
-      self._source.connect(self._gainNode);
+      self._musicBuffer = decodedBuffer;
+      self._setup(decodedBuffer);
+      self._currentTime = 0;
+      self._status = 'pause';
 
       callback();
     });
   };
 
+  /* 音声を再生する
+   * time: 音声の再生開始位置（秒）
+  ----------------------------------------------------------*/
   proto.play = function(time) {
     if (this._source) {
       if (typeof time === 'undefined') {
@@ -42,23 +84,45 @@ var AudioController = (function() {
 
       this._startTime = Date.now()/1000 - time;
       this._source.start(0, time);
+
+      this._status = 'play';
     }
   };
 
+  /* 音声を一時停止する
+  ----------------------------------------------------------*/
   proto.pause = function() {
-
+    this._currentTime = Date.now()/1000 - this._startTime;
+    this._status = 'pause';
+    this.stop();
+    this._setup(this._musicBuffer);
   };
 
+  /* 音声を停止する
+   * NOTE: 作成中。この関数の必要性はある？
+  ----------------------------------------------------------*/
   proto.stop = function() {
-    if (this.source) {
-      this.source.stop();
+    if (this._source) {
+      this._source.stop();
     }
   };
 
-  proto.seek = function() {
+  /* 音声の再生位置の変更
+   * time: この秒数に再生位置を変更
+  ----------------------------------------------------------*/
+  proto.seek = function(time) {
+    this._currentTime = time;
 
+    if (this._status === 'play') {
+      this._setup(this._musicBuffer);
+      this.play(time);
+    }
   };
 
+  /* 音声のボリュームの取得/変更
+   * volumeが指定されていない場合、音声のボリュームを返却
+   * volume: 0~1.0でボリュームを指定
+  ----------------------------------------------------------*/
   proto.volume = function(volume) {
     if (typeof volume === 'undefined') {
       return this._gainNode.gain.value;
@@ -67,6 +131,13 @@ var AudioController = (function() {
     this._gainNode.gain.value = volume;
   };
 
+  /* 音声の情報を返却
+   * 情報取得に失敗した際はnullを返却
+   * 以下返却情報
+   * currentTime: 音声の現在再生位置（秒）
+   * totalTime: 音声の総秒数
+   * status: 現在ステータス
+  ----------------------------------------------------------*/
   proto.audio = function() {
     var self = this;
 
@@ -76,19 +147,25 @@ var AudioController = (function() {
 
     var audio = {
       currentTime: (function() {
-        if (self._startTime === null) {
-          return null;
-        }
+        if (self._status === 'play') {
+          if (self._startTime === null) {
+            return null;
+          }
 
-        return Date.now()/1000 - self._startTime;
+          return Date.now()/1000 - self._startTime;
+        } else if (self._status === 'pause') {
+          return self._currentTime;
+        }
       })(),
       totalTime: self._source.buffer.duration,
-      status: null
+      status: self._status
     };
 
     return audio;
   };
 
+  /* 音声の周波数情報を配列で返却
+  ----------------------------------------------------------*/
   proto.getSpectrums = function() {
     var spectrums = new Uint8Array(this._analyzer.frequencyBinCount);
     this._analyzer.getByteFrequencyData(spectrums);
--- html/public/posts/js_audio_analyzer/demo/main.js
+++ html/public/posts/js_audio_analyzer/demo/main.js
@@ -2,9 +2,30 @@ $(function() {
   var animationId;
   var $svg = $('.m-svg01');
   var $line = $svg.find('.m-svg01__line');
+  var $playBtn = $('.m-player01.js-player .m-player01__playBtn');
+  var $volume = $('.m-player01.js-volume');
 
   var audioController = new AudioController();
-  audioController.volume(0.75);
+  audioController.volume(0.25);
+  $('.m-player01.js-volume').find('.m-bar01__current').css('width', '25%');
+
+  $playBtn.on('click', function() {
+    var audio = audioController.audio();
+    var $playIcon = $playBtn.find('.fa');
+
+    if (audio.status === 'play') {
+      audioController.pause();
+      $playIcon.addClass('fa-play');
+      $playIcon.removeClass('fa-pause');
+    }
+    if (audio.status === 'pause') {
+      audioController.play(audio.currentTime);
+      $playIcon.addClass('fa-pause');
+      $playIcon.removeClass('fa-play');
+    }
+
+    return false;
+  });
 
   var render = function() {
     var spectrums = audioController.getSpectrums();
@@ -16,6 +37,12 @@ $(function() {
     }
     pointStr += '640,0';
     $line.attr('points', pointStr);
+
+    var currentTime = audioController.audio().currentTime;
+    var totalTime = audioController.audio().totalTime;
+    var currentP = currentTime / totalTime * 100;
+
+    $('.m-player01.js-player .m-bar01__current').css('width', currentP + '%');
 
     animationId = requestAnimationFrame(render);
   };
@@ -55,4 +82,46 @@ $(function() {
       }
     });
   })();
+
+  /* m-bar01
+   * data属性にonClickが指定されていると、クリック時に指定された関数を実行
+   * ex:
+   * $('#btn .m-bar01').data('onClick', function(percent) {
+   *  // 処理
+   * });
+  ----------------------------------------------------------*/
+  (function() {
+    var $components = $('.m-bar01');
+
+    $components.each(function() {
+      var $component = $(this);
+      var $current = $component.find('.m-bar01__current');
+
+      $component.on('click', function(e) {
+        var componentW = $component.outerWidth();
+        var clickL = e.offsetX;
+        var percent = clickL / componentW;
+        $current.css('width', percent * 100 + '%');
+
+        var callback = $component.data('onClick');
+        if (callback) {
+          callback(percent);
+        }
+      });
+    });
+  })();
+
+  /* m-bar01のコールバック指定
+  ----------------------------------------------------------*/
+  (function() {
+    $('.m-player01.js-player .m-bar01').data('onClick', function(percent) {
+      var targetTime = audioController.audio().totalTime * percent;
+      audioController.seek(targetTime);
+    });
+
+    $('.m-player01.js-volume .m-bar01').data('onClick', function(percent) {
+      audioController.volume(percent);
+    });
+  })();
+
 });
